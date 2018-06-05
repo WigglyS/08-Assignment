165fb7683ffd6cca8b4f64d883394c6c7c1ea238
TRAVIS BODINE <TRABODIN@uat.edu>
2018-06-05 16:18:14 -0700

    multi file drifting!

---
+++ Assignment8/Assignment8/Source.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "iostream"
+#include "string"
+#include "ctime"
+#include "vector"
+using std::cout;
+using std::endl;
+
+int return1()
+{
+	cout << 1 << endl;
+	return 1;
+}
